3f56b5760b93a6608481dafe86ebc5df11aeb0c5
only-zover <j0seextremehd@outlook.com>
2023-01-13 16:39:55 -0300

    display estatico

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".sub-bt");
 const sendBt = document.querySelector(".send-bt");
 const modal = document.querySelector("dialog");
 
-const countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+const CountDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
 
 button.addEventListener("click", () => {
   modal.showModal();
@@ -16,16 +16,14 @@ sendBt.addEventListener("click", () => {
 let countDown = setInterval(function () {
   let now = new Date().getTime();
 
-  let distance = countDownDate - now;
+  let distance = CountDownDate - now;
 
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  countDownText.innerText = `${("000" + days).slice(-3)} : ${(
-    "00" + hours
-  ).slice(-2)} : ${("00" + minutes).slice(-2)} : ${("00" + seconds).slice(-2)}`;
+  countDownText.innerText = `${("000" + days).slice(-3)} : ${("00" + hours).slice(-2)} : ${("00" + minutes).slice(-2)} : ${("00" + seconds).slice(-2)}`;
 
   if (distance < 0) {
     clearInterval(countDown);
